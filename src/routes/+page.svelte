a978e0b0197660716c5246c56bcd33dc8161a507
statikana <86629697+statikana@users.noreply.github.com>
2026-02-12 21:54:50 -0500

    rearrange

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script>
     import Terminal from "../Terminal.svelte";
+    function toggleTheme() {
+        
+    }
 </script>
 
 <svelte:head>
     <script src="https://kit.fontawesome.com/e469a2ed37.js" crossorigin="anonymous"></script>
 </svelte:head>
 
+<div id="theme-toggle-div">
+    <!-- svelte-ignore a11y_consider_explicit_label -->
+    <!-- <button id="theme-toggle-btn" onclick={toggleTheme}>
+        <i class="fa-regular fa-sun"></i>
+    </button> -->
+</div>
 <div id="main-personal">
     <div id="me">
         <p id=name class=glow>
@@ -22,7 +31,7 @@
             Computer Engineering<br>
             University of Michigan
             <br>
-            <span>class of 2029</span>
+            <span>Class of 2029</span>
         </p>
         <div id="links">
             <a class="social-link" href="https://github.com/statikana" aria-label="github" target="_blank"><i class="fa-brands fa-github"></i></a>
@@ -30,52 +39,54 @@
             <a class="social-link" href="https://www.instagram.com/that.ryguy/" aria-label="instagram" target="_blank"><i class="fa-brands fa-instagram"></i></a>
         </div>  
     </div>
-
-    <div id=terminal-container>
-        <details id=terminal-details open>
-            <summary>my super cool terminal <a href="/terminal"> [click for fullscreen]</a></summary>
-            <Terminal />
-        </details>
-    </div>
-
-    <h2 class="titlebar glow">My Stuff</h2>
-
-    <div class="center" id="projects">
-        <div class="project">
-            <p>This Site</p>
-            <div class="project-desc">
-                Of course, I made this site using HTML, CSS, and JS. I also used <a href="https://svelte.dev/" target="_blank">Svelte</a> for routing and 
-                dynamic rendering. To protect my privacy, this site's code is not publicly linked to my github, but you are free to inspect the page source.
-            </div>
+    <div class="row-shell">
+        <div id=terminal-container>
+            <details id=terminal-details open>
+                <summary>my super cool terminal <a href="/terminal"> [click for fullscreen]</a></summary>
+                <Terminal />
+            </details>
         </div>
-        <hr class="line-break">
-        <div class="project">
-            <p>PyTenno [<a href="https://github.com/statikana/PyTenno" target=_blank>Source</a>]</p>
-            <div class="project-desc">
-                A relatively simple asynchonous HTTPS wrapper for <a href="https://warframe.market/" target="_blank">warframe.market</a>, a website where users can
-                buy and sell items in the video game <a href="https://www.warframe.com/game/about" target="_blank">Warframe</a>. Written in Python, it translates API endpoints
-                and JSON structures into Python functions and data structures. It was my first package which I published on <a href="https://pypi.org/project/pytenno/" target=_blank>PyPI</a> (Python's Package Index). 
-                It includes comprehensive documentation on <a href="https://pytenno.readthedocs.io/en/latest/index.html" target="_blank">readthedocs.io</a>, though it isn't maintained any more.
-            </div>
-        </div>
-        <hr class="line-break">
-        <div class="project">
-            <p>Download Manager [<a href="https://github.com/statikana/dlm" target=_blank>Source</a>]</p>
-            <div class="project-desc">
-                Also made in Python, I used this to explore multithreading. It uses concurrent threads on your device to read and write data from a website (e.g., a file server) faster than you are normally able to.
-                Each thread reads a different segment of the source file, and writes to the associated segment of the new file being created on your computer. Uses file locks to
-                prevent race conditions. Shows substantial improvements in download times, depending on the file size and the number of concurrent threads used.
-            </div>
-        </div>
-        <hr class="line-break">
-        <div class="project">
-            <p>Vanir [<a href="https://github.com/statikana/Vanir" target=_blank>Source</a>]</p>
-            <div class="project-desc">
-                One of my first substantial projects. Uses <a href=https://discord.com/ target=_blank>Discord</a>'s API to create an interactive bot-agent which users
-                can send commands to. Notable features include: <ul>
-                    <li>An SQL-based currency system with robust normalization</li>
-                    <li>Safe, sandboxed arbitrary code execution for many languages via <a href=https://github.com/engineer-man/piston target=_blank>Piston</a></li>
-                </ul> 
+        <div class=spacer></div>
+        <div class="col-shell">
+            <h2 class="titlebar glow">My Stuff</h2>
+            <div class="center" id="projects">
+                <div class="project">
+                    <p>This Site</p>
+                    <div class="project-desc">
+                        Of course, I made this site using HTML, CSS, and JS. I also used <a href="https://svelte.dev/" target="_blank">Svelte</a> for routing and 
+                        dynamic rendering. To protect my privacy, this site's code is not publicly linked to my github, but you are free to inspect the page source.
+                    </div>
+                </div>
+                <hr class="line-break">
+                <div class="project">
+                    <p>PyTenno [<a href="https://github.com/statikana/PyTenno" target=_blank>Source</a>]</p>
+                    <div class="project-desc">
+                        A relatively simple asynchonous HTTPS wrapper for <a href="https://warframe.market/" target="_blank">warframe.market</a>, a website where users can
+                        buy and sell items in the video game <a href="https://www.warframe.com/game/about" target="_blank">Warframe</a>. Written in Python, it translates API endpoints
+                        and JSON structures into Python functions and data structures. It was my first package which I published on <a href="https://pypi.org/project/pytenno/" target=_blank>PyPI</a> (Python's Package Index). 
+                        It includes comprehensive documentation on <a href="https://pytenno.readthedocs.io/en/latest/index.html" target="_blank">readthedocs.io</a>, though it isn't maintained any more.
+                    </div>
+                </div>
+                <hr class="line-break">
+                <div class="project">
+                    <p>Download Manager [<a href="https://github.com/statikana/dlm" target=_blank>Source</a>]</p>
+                    <div class="project-desc">
+                        Also made in Python, I used this to explore multithreading. It uses concurrent threads on your device to read and write data from a website (e.g., a file server) faster than you are normally able to.
+                        Each thread reads a different segment of the source file, and writes to the associated segment of the new file being created on your computer. Uses file locks to
+                        prevent race conditions. Shows substantial improvements in download times, depending on the file size and the number of concurrent threads used.
+                    </div>
+                </div>
+                <hr class="line-break">
+                <div class="project">
+                    <p>Vanir [<a href="https://github.com/statikana/Vanir" target=_blank>Source</a>]</p>
+                    <div class="project-desc">
+                        One of my first substantial projects. Uses <a href=https://discord.com/ target=_blank>Discord</a>'s API to create an interactive bot-agent which users
+                        can send commands to. Notable features include: <ul>
+                            <li>An SQL-based currency system with robust normalization</li>
+                            <li>Safe, sandboxed arbitrary code execution for many languages via <a href=https://github.com/engineer-man/piston target=_blank>Piston</a></li>
+                        </ul> 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -95,8 +106,8 @@
         overflow-y: scroll;
         overflow-x: hidden;
 
-        width: 90%;
-
+        flex: .9;
+        margin-left: 2%;
 
         scrollbar-color: #fff #0000;
         scrollbar-arrow-color: #0000;
@@ -113,17 +124,48 @@
     #main-personal {
         display: flex;
         flex-direction: column;
-        align-items: center;
+    }
+
+    #theme-toggle-btn {
+        justify-self: left;
+        background-color: inherit;
+        color: #fff;
+        font-size: 125%;
+        box-shadow: none;
+        border: none;
+        margin: 1%;
+        padding: 0;
+    }
+
+    
+    #theme-toggle-btn :hover {
+        text-shadow: 0 0 10px #fff8;
+    }
+
+    .spacer {
+        flex: .05;
     }
 
     .glow {
         color: #fff;
-        text-shadow: 0 0 10px #fff8;
+        /* text-shadow: 0 0 10px #fff8; */
     }
 
     .titlebar {
         margin-top: 3%;
         margin-bottom: 0;
+    }
+
+    .row-shell {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .col-shell {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
     ul {
@@ -133,6 +175,7 @@
     div#me {
         border-style: hidden;
         margin: 5%;
+        /* margin-top: 0; */
 
         text-align: center;
         color: #fff;
@@ -174,13 +217,12 @@
     div.center {
         margin-bottom: 2%;
 
+
         color: #fff;
         padding: 1%;
-        width: 90%;
     }
 
     hr.line-break {
-        width: 90%
     }
 
     div.project > div.project-desc {
