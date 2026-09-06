24cc2bad21e82e4ac9a1f257b48e60203e4db133
unknown <ryanoc1026@gmail.com>
2026-09-06 10:37:58 -0400

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -106,7 +106,6 @@
                 </div>
                 <hr class="line-break">
 
-                ... as I gain more experience, I'll highlight more projects I'm proud of here. I have a whole slew of small, unpolished side-projects public on <a href="https://github.com/statikana">my GitHub</a> as well.
 
                 <!-- pytenno -->
                 <!-- <div class="project">
@@ -142,6 +141,8 @@
                         </ul> 
                     </div>
                 </div> -->
+                ... as I gain more experience, I'll highlight more projects I'm proud of here. I have a whole slew of small, unpolished side-projects public on <a href="https://github.com/statikana">my GitHub</a> as well.
+
             </div>
         </div>
     </div>
